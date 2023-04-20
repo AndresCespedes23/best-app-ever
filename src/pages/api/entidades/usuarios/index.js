88e8868eba3c1cbcdef4ab4158006ceba5e05f0e
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-04-19 21:29:50 -0300

    add users model to the API endpoint

---
+++ src/pages/api/entidades/usuarios/index.js
@@ -0,0 +1,48 @@
+const usuarios = [
+  {
+    id: "1",
+    nombre: "Juan Jose",
+    apellido: "Frijol",
+    fechaNac: "03/10/1993",
+    direccion: "Av. de las Camelias 1976",
+    ciudad: "Rosario",
+    provincia: "Santa Fe",
+    contraseña: "123456789",
+    mail: "juanjo@gmail.com",
+    telefono: "123456789",
+    tipo: "",
+    isActive: true,
+  },
+  {
+    id: "2",
+    nombre: "Juan Carlos",
+    apellido: "Frijol",
+    fechaNac: "03/10/1993",
+    direccion: "Av. de las Camelias 1992",
+    ciudad: "Rosario",
+    provincia: "Santa Fe",
+    contraseña: "123456789",
+    mail: "juancar@gmail.com",
+    telefono: "123456789",
+    tipo: "",
+    isActive: true,
+  },
+  {
+    id: "3",
+    nombre: "Facundo",
+    apellido: "Frijol",
+    fechaNac: "03/10/1993",
+    direccion: "Av. de las Camelias 1986",
+    ciudad: "Rosario",
+    provincia: "Santa Fe",
+    contraseña: "123456789",
+    mail: "facuca@gmail.com",
+    telefono: "123456789",
+    tipo: "",
+    isActive: true,
+  },
+];
+
+export default function handler(req, res) {
+  res.status(200).json(usuarios);
+}
