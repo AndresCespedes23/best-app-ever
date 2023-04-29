1b021617b45407ed54554fbe280cdfdcc2affbd4
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-04-29 00:38:48 -0300

    change image for return arrow component

--- src/Components/ReturnArrow/index.js
+++ src/Components/ReturnArrow/index.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import arrow from "../../../public/images/store/volver.png";
+import arrow from "../../../public/images/store/retornar.png";
 import Image from "next/image";
 import styles from "../../styles/return-arrow.module.css";
 export default function ReturnArrow({ href }) {
   return (
     <div className={styles.arrow}>
-      <Image height={25} width={25} src={arrow} alt="volver" />
+      <Image height={25} width={25} src={arrow} alt="retornar" />
       <Link className={styles.arrowText} href={href}>
         <p>Volver</p>
       </Link>
