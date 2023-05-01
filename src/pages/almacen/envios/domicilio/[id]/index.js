bd580f0180da87265a5b22ceeb4f438077a6840f
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2023-05-01 12:32:16 -0300

    add arrow component to go back to previous page

--- src/pages/almacen/envios/domicilio/[id]/index.js
+++ src/pages/almacen/envios/domicilio/[id]/index.js
@@ -1,6 +1,12 @@
+import ReturnArrow from "@/Components/ReturnArrow";
 import { useRouter } from "next/router";
 export default function Detalles() {
   const router = useRouter();
   const { id } = router.query;
-  return <h1>Aca vamos a ver los detalles del envío n°: {id}</h1>;
+  return (
+    <>
+      <ReturnArrow href="/almacen/envios/domicilio" />
+      <h1>Aca vamos a ver los detalles del envío n°: {id}</h1>
+    </>
+  );
 }
