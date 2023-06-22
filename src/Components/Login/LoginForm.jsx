6df11a4afc337beb237c41ee3d66f96defd528eb
riveroide <nicolasrivero1994@gmail.com>
2023-06-22 15:59:25 -0400

    login funcional SIN ESTILOS

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,58 +1,18 @@
-import React, { useState } from 'react';
-
-export default function LoginForm() {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [profilePic, setProfilePic] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const userData = { fullName, email, profilePic };
-
-    try {
-      // Make a POST request to the API route
-      const response = await fetch('/api/saveUser', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(userData),
-      });
-
-      if (response.ok) {
-        console.log('User data saved successfully');
-        // Perform any additional actions or navigate to a new page
-      } else {
-        console.error('Error saving user data:', response.statusText);
-        // Handle the error appropriately
-      }
-    } catch (error) {
-      console.error('Error saving user data:', error);
-      // Handle the error appropriately
-    }
+import { useSession, signIn, signOut } from "next-auth/react"
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
   }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-      
-        type="text"
-        placeholder="Full Name"
-        value={fullName}
-        onChange={(e) => setFullName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Profile Picture URL"
-        value={profilePic}
-        onChange={(e) => setProfilePic(e.target.value)}
-      />
-      <button type="submit">Save User</button>
-    </form>
-  );
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 }
